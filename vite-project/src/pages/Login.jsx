3b5bd7482c473e4624b0d0fa9ff1b632e6fe6711
GBTorto <moraisgabriel867@gmail.com>
2025-08-06 16:53:24 -0300

    lógica do login está funcionando, falta apenas colocar a rota

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { supabase } from "../../supabase/supabaseClient";
 
 const Login = () => {
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
 
-    const supabase_function = async () => {
+    const Logar = async () => {
         const {data, error} = await supabase
-            .from()
-            .select()
+            .from("professor")
+            .select("*")
+            .eq("email", email)
+            .eq("senha", password)
+
+            if (error || !data){
+                alert("email invalido ou senha invalidos")
+            }
+            else{
+                console.log("tudo certo")
+            }
     }
+
+    
+
     return (
         <div className="login">
             <div className="login__email">
                 <label htmlFor="login__email">Digite seu email:
-                    <input type="text" email="email" id="login__email" />
+                    <input type="text" email="email" id="login__email" value={email} onChange={e => setEmail(e.target.value)} />
                 </label>
             </div>
 
             <div className="login__password">
                 <label htmlFor="login__password">Digite sua senha:
-                    <input type="password" name="password" id="login__password" />
+                    <input type="password" name="password" id="login__password" value={password} onChange={e => setPassword(e.target.value)}/>
                 </label>
+            </div>
+
+            <div className="login__btn">
+                <button className="login__btn--style" onClick={Logar}>
+                    <p>Logar</p>
+                </button>
             </div>
         </div>
     )
